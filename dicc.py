2b66264a26c983cf6a22292cdeabb311fa3ba2cf
PaolaGT246 <a.23308051281187@cbtis128.edu.mx>
2024-10-19 16:12:21 -0600

    Add files via upload

---
+++ dicc.py
@@ -0,0 +1,28 @@
+#se crea un diccionario vacío
+print(" ")
+print("Gutierrez Torres Paola")
+print(" ")
+
+persona = {}
+#le pedimos al usuario ingresar sus datos
+nombre = input("Introduce tu nombre: ")
+persona['nombre'] = nombre
+print(persona)
+
+edad = input("Introduce tu edad: ")
+persona['edad'] = edad
+print(persona)
+
+sexo = input("Introduce tu sexo (masculino/femenino): ")
+persona['sexo'] = sexo
+print(persona)
+
+telefono = input("Introduce tu teléfono: ")
+persona['telefono'] = telefono
+print(persona)
+
+correo = input("Introduce tu correo electrónico: ")
+persona['correo'] = correo
+print(persona)
+
+#el diccionrio tiene que presentarse completo en pantalla
